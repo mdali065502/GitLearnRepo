7f907b5875623d0c37900c16b3ab210751732ea9
Md Ali <iamalikhan065502@gmail.com>
2021-05-05 12:55:07 +0530

    initail commit

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+print(hello world)
+SyntaxError: invalid syntax
+>>> print("hello World")
+hello World
+>>> 
